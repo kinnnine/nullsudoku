208a9e562e42bfa6abcba68151d3f8bc97897bfd
Supphakit Duanghoy <nakaharaml9@gmail.com>
2026-07-23 20:50:13 +0000

    workbox: include html, css and map for offline caching

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'build',
 	globPatterns: [
-		'**/*.{txt,json,png,ico}'
+		'**/*.{txt,json,png,ico,html,css,map}'
 	],
 	swDest: 'build/service-worker.js',
 	ignoreURLParametersMatching: [
